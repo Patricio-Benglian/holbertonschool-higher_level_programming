c9b96e5a99c8afa82e77f65e003df4c0341c79b7
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-30 07:19:01 -0700

    Print 0-99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    comma = ""
-    if number != 0:
-        comma = ", "
-    print("{1}{0:02d}".format(number, comma), end="")
-print("\n")
+    if number != 99:
+        print("{:02d}".format(number), end=", ")
+    else:
+        print(number)
