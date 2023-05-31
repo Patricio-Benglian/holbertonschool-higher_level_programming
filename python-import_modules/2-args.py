a3cbc23451dc885da24ba3794bb72a57b45eb7dd
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-31 04:13:29 -0700

    Conditional for no args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 import sys
 if __name__ == "__main__":
     i = 1
-    print("{} arguments.".format(len(sys.argv) - 1))
+    if (len(sys.argv) == 1):
+        print("{} arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for arguments in sys.argv[1:]:
         print("{}: {}".format(i, arguments))
         i += 1
