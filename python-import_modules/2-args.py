8a3a84534297277491d3adf507f75a95abf23a13
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-31 04:11:33 -0700

    Count starts at 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    i = 0
+    i = 1
     print("{} arguments.".format(len(sys.argv) - 1))
     for arguments in sys.argv[1:]:
         print("{}: {}".format(i, arguments))
