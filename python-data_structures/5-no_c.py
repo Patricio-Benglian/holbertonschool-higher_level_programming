c6bc85bd5a786f70648eea21fecccb5349a73d1a
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-05 07:45:10 -0700

    No C

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    cLess_string = ""
+    for letter in my_string:
+        if letter != 'c' and letter != 'C':
+            cLess_string += letter
+    return cLess_string
