fb651a6daa19578ab191b5f73748f7cf82b9a319
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-05 11:04:17 -0700

    Used Eval for shorter code

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    iterator = ['a', 'b']
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    if len(list_a) < 2:
-        for i in range(len(list_a), 2):
-            list_a.append(0)
-    if len(list_b) < 2:
-        for i in range(len(list_b), 2):
-            list_b.append(0)
+    for z in iterator:
+        if len(eval("list_" + z)) < 2:
+            for i in range(len(eval("list_" + z)), 2):
+                eval("list_" + z).append(0)
     list_c = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
     tuple_c = tuple(list_c)
     return (tuple_c)
