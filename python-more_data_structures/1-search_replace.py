421ebf6b22c59b693229b8db266624587d71582d
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-06 11:02:36 -0700

    Reduced code length

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [element if element !=
-                search else replace for element in my_list]
-    return new_list
+    return [element if element !=
+            search else replace for element in my_list]
