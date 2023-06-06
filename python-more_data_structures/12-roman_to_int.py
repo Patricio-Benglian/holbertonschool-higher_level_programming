53834c3476fec62e0a71eec8d2754816ff77aa0d
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-06 12:20:35 -0700

    Roman to Int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and type(roman_string) is str:
-        converter = {'M': 1000, 'D': 500, 'C': 100,
-                     'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        decimal = 0
-        for number in roman_string:
-            if converter.get(number):
-                if decimal < converter[number]:
-                    decimal *= -1
-                if converter[number]:
-                    decimal += converter[number]
-            else:
-                return 0
-        return decimal
-    return 0
+    if not roman_string or type(roman_string) is not str:
+        return 0
+    converter = {'M': 1000, 'D': 500, 'C': 100,
+                 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    decimal = 0
+    for number in roman_string:
+        if converter.get(number):
+            if decimal < converter[number]:
+                decimal *= -1
+            if converter[number]:
+                decimal += converter[number]
+        else:
+            return 0
+    return decimal
