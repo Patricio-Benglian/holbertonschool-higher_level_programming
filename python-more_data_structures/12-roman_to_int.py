489872af69fc7a5616ac83ba1c087fdd4762fafb
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-06 11:40:24 -0700

    Roman to Int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    converter = {'M': 1000, 'D': 500, 'C': 100,
+                 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    decimal = 0
+    for number in roman_string:
+        if decimal < converter[number]:
+            decimal *= -1
+        if converter[number]:
+            decimal += converter[number]
+    return decimal
