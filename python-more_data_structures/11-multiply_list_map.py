264adf566b072a279757bf9254dbb4dae0e304af
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-06 10:45:47 -0700

    Multiply List

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multList = [element * 2 for element in my_list]
-    return multList
+    return [element * 2 for element in my_list]
