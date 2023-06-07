f556088e44016c6ce64ddeb1b718ea2a4f2cbaef
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-07 05:22:43 -0700

    Roman to Int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,24 +2,19 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) is not str:
         return 0
-    converter = {"M": 1000, "D": 500, "C": 100,
-                 "L": 50, "X": 10, "V": 5, "I": 1}
+    a = roman_string
+    conv = {"M": 1000, "D": 500, "C": 100,
+            "L": 50, "X": 10, "V": 5, "I": 1}
     output = []
     decimal = 0
-    # lol. lmao, even
-    hardcode = {"LXIX": 69, "LXXIX": 79, "LXXXIX": 89, "DCIX": 609,
-                "DCXIX": 619, "DCXXIX": 629, "DCXXXIX": 639,
-                "DCXLIX": 649, "DCLIX": 659}
-    if hardcode.get(roman_string):
-        return hardcode[roman_string]
-    for number in roman_string:
-        if converter.get(number):
+    for ind, num in enumerate(a):
+        if conv.get(num):
             # if subtraction, subtract current sum from current value
-            if decimal < converter[number] and decimal != 0:
-                output.append(converter[number] - decimal)
+            if ind != len(a) - 1 and conv[a[ind + 1]] > conv[num]:
+                output.append(conv[num] * -1)
                 decimal = 0
             else:
-                decimal += converter[number]
+                output.append(conv[num])
         else:
             return 0
     output.append(decimal)
