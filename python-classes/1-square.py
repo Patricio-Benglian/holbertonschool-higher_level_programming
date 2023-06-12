40760f776be17f3eb1dd2c15d931c1bca6e279dd
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-12 06:31:29 -0700

    Init Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines Square"""
+
+
+class Square:
+    """defines Square with size"""
+
+    def __init__(self, size=0):
+        """initiates size value"""
+        self.__size = size
