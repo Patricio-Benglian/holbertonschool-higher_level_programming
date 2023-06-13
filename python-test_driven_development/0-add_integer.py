bf583c83b8c0c18cad3f70ca491367c300f561e1
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-13 10:14:50 -0700

    Add Int

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,14 +7,9 @@ lines
 
 
 def add_integer(a, b=98):
-    """ Add integer
-    >>> add_integer(3, 5.5)
-    8
-    >>> add_integer("4", 5)
-    Traceback (most recent call last):
-    ...
-    TypeError: a must be an integer
     """
+    >>> add_integer(3, 5.5)
+    8 """
     if (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
     elif (type(b) != int and type(b) != float):
