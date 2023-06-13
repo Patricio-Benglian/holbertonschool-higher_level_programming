608ef0bc4e7015721d9151c8947ce1e496314655
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-13 05:34:25 -0700

    Ikdk anymore

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,6 +14,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (type(position[0]) != int or type(position[1]) != int):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(position[0]) < 0 or type(position[1]) < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
 
