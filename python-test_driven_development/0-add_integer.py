bc1a2c578106b0df269b7622c3fde2bb3c7c5408
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-13 09:35:21 -0700

    Idk

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,3 +10,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     else:
         return int(a)+int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod
