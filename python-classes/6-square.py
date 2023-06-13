4a98e69db8a3bd8a1fe530f60e603b1f1336d6b7
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-13 05:37:02 -0700

    Ikdk anymore

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,16 +6,8 @@ class Square:
     """defines Square with size"""
 
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if (type(position) != tuple or len(position) != 2):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(position[0]) != int or type(position[1]) != int):
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
-        self.__position = position
+        self.position = position
 
     """ Returns size """
     @property
