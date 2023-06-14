4e4fe85a9397062b3378e653ce79b5b67e76ae00
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-14 13:24:33 -0700

    Text Indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Text_indentation module
+'''
+
+
+def errorCheck(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+def text_indentation(text):
+    '''
+    Indents text on . : or ?
+    '''
+    errorCheck(text)
+    flag = 0
+    for index, c in enumerate(text):
+        if flag == 0:
+            print("{:s}".format(c), end="")
+            flag = 0
+        else:
+            flag = 0
+        if c == '.' or c == ':' or c == '?':
+            print("\n")
+            if text[index + 1] == ' ':
+                flag = 1
