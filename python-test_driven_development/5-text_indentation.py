54e10b7d7e964d261ad339f37d10cdbb7365b058
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-14 13:38:09 -0700

    Reworked replace because of confusing conditionals

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,14 +14,8 @@ def text_indentation(text):
     Indents text on . : or ?
     '''
     errorCheck(text)
-    flag = 0
-    for index, c in enumerate(text):
-        if flag == 0:
-            print("{:s}".format(c), end="")
-            flag = 0
-        else:
-            flag = 0
-        if c == '.' or c == ':' or c == '?':
-            print("\n")
-            if text[index + 1] == ' ':
-                flag = 1
+    text = text.replace(". ", ".\n\n")
+    text = text.replace(": ", ":\n\n")
+    text = text.replace("? ", "?\n\n")
+    print (text, end="")
+        
