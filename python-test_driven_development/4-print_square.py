4efa77881763ee53fbc2c84e3798dedfa5ea5720
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-14 12:18:25 -0700

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+4-print_square module
+'''
+def checkErrors(size):
+    '''
+    Checks for input errors
+    '''
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+def print_square(size):
+    '''
+    prints a square of size
+    '''
+    checkErrors(size)
+    for i in range(size):
+        print("{}".format("#" * size))
