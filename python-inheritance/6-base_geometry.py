ac221d9a619032d17df8bf2c383e94fef4060508
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-19 11:18:44 -0700

    Based Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+base_geometry model
+"""
+
+
+class BaseGeometry:
+    """
+    base class for geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
