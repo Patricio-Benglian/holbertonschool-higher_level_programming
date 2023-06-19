834f8b5d85dc8026125cf5b8eacb6e40588d5537
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-19 11:15:12 -0700

    Inherit  from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+inherits_from module
+"""
+def inherits_from(obj, a_class):
+    """
+    Returns true if obj is subclass of class
+    """
+    return isinstance(obj, a_class) if not type(obj) is a_class else False
