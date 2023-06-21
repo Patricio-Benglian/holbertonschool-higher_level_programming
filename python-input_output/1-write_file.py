5a6a22ca79bb439057a14ec8753541ffa49d96dc
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-21 06:45:03 -0700

    Write File

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+1-write_file module
+"""
 def write_file(filename="", text=""):
+    """
+    writes into file
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
+    
