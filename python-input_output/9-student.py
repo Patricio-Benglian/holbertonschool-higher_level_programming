5b72fce96439d5b329b251f122b715c6c5d74abc
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-21 10:51:46 -0700

    Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,11 +5,20 @@
 
 
 class Student:
+    """
+    Student Class with names and age
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Inits values
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        returns json format
+        """
         return self.__dict__
 
