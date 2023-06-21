da8308aaeb97c239634694a179c3096d326648f2
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-21 09:52:36 -0700

    Add Item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """
 7-add_item module
 """
-import json
 from sys import argv
 
 
@@ -10,7 +9,7 @@ saveJSON = __import__('5-save_to_json_file').save_to_json_file
 loadJSON = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    listo = loadJSON("add_item.json") + argv[1:]
+    listo = loadJSON("add_item.json")
 except Exception:
-    listo = argv[1:]
-saveJSON(listo, "add_item.json")
+    listo = []
+saveJSON(listo + argv[1:], "add_item.json")
