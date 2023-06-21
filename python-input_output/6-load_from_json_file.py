39ce0dc0a7d8425e6707111809b384caefbbac2b
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-21 07:36:52 -0700

    Load from JSON

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,4 +10,5 @@ def load_from_json_file(filename):
     loads object from JSON file
     """
     with open(filename, mode="r", encoding="utf-8") as f:
-        return json.loads(f)
+        return json.loads(f.read())
+    
