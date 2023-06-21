88dc7f48eb6d1e59654c4ec73caffe28227d256f
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-21 09:34:02 -0700

    Add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,13 @@ from sys import argv
 saveJSON = __import__('5-save_to_json_file').save_to_json_file
 loadJSON = __import__('6-load_from_json_file').load_from_json_file
 
+"""
+HOLA
+"""
+
 
 listo = loadJSON("add_item.json") + argv[1:]
+"""
+AAAAAAAAAAAAAAAAAA
+"""
 saveJSON(listo, "add_item.json")
