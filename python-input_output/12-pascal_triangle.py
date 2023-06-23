7e8c4462feabd5860c9464d7decf58e87c711b4e
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-23 10:36:03 -0700

    Pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,10 +12,11 @@ def pascal_triangle(n):
         return []
     triangle = [[1]]
     for i in range(0, n - 1):
-        aList = [
-            (0 if m == 0 else triangle[i][m - 1])
-            + (0 if m == (len(triangle[i])) else triangle[i][m])
-            for m in range(len(triangle[i]) + 1)
-        ]
-        triangle.append(aList)
+        triangle.append(
+            [
+                (0 if m == 0 else triangle[i][m - 1])
+                + (0 if m == (len(triangle[i])) else triangle[i][m])
+                for m in range(len(triangle[i]) + 1)
+            ]
+        )
     return triangle
