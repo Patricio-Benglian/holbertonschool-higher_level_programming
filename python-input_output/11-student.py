b137b7d520ce1a8070cdcb38360f3ec98f014122
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-23 09:06:32 -0700

    Student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+9-student module
+"""
+
+
+class Student:
+    """
+    Student Class with names and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Inits values
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns json format
+        """
+        if attrs is not None:
+            return {
+                attribute: getattr(self, attribute)
+                for attribute in attrs
+                if hasattr(self, attribute)
+            }
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        reloads attrs from json
+        """
+
+        for attr in json:
+            setattr(self, attr, json.get(attr))
