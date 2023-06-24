938bebecaa62aa8d2f33cc6652126ef95d84b44b
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-24 11:50:03 -0700

    Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+base_geometry model
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square based off Rectangle class
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def __str__(self):
+        """Returns description square"""
+        return f"[Square] {self.__size}/{self.__size}"
