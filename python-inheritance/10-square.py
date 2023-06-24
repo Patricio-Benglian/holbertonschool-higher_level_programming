cf165aea6d84d9008aac17498411b7ad8ea93da6
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-24 11:46:28 -0700

    Square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     Square based off Rectangle class
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
