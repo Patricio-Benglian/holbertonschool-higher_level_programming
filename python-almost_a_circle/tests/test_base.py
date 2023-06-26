07d6f601274c5d6b62927d6625cabc6ea8ef5e41
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-26 06:56:51 -0700

    .

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -3,3 +3,13 @@
 test module
 """
 import unittest
+from models.base import Base
+
+class test_Base(unittest.TestCase):
+    """
+    tests and stuff
+    """
+    def test_is_instance(self):
+        """Check if creates"""
+        instance = Base()
+        self.assertTrue(isinstance(instance, Base))
