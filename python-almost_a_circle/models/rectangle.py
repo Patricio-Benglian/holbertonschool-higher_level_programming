8a911e4f562bf656544351d389f7c6c84494c4f5
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-26 05:19:15 -0700

    d

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.dimVal("width", width)
-        self.dimVal("height", height)
-        self.posVal("x", x)
-        self.posVal("y", y)
+        # self.dimVal("width", width)
+        # self.dimVal("height", height)
+        # self.posVal("x", x)
+        # self.posVal("y", y)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -56,14 +56,14 @@ class Rectangle(Base):
         self.posVal("y", y)
         self.__y = y
 
-    def dimVal(self, name, arg):
-        if type(arg) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if arg < 1:
-            raise ValueError(f"{name} must be > 0")
+    # def dimVal(self, name, arg):
+    #     if type(arg) is not int:
+    #         raise TypeError(f"{name} must be an integer")
+    #     if arg < 1:
+    #         raise ValueError(f"{name} must be > 0")
 
-    def posVal(self, name, arg):
-        if type(arg) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if arg < 0:
-            raise ValueError(f"{name} must be >= 0")
+    # def posVal(self, name, arg):
+    #     if type(arg) is not int:
+    #         raise TypeError(f"{name} must be an integer")
+    #     if arg < 0:
+    #         raise ValueError(f"{name} must be >= 0")
