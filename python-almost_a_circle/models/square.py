0c590ec194e7282b1d9df079d5de74e990158e3b
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-27 06:26:50 -0700

    getters

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,18 @@ class Square(Rectangle):
     def __str__(self):
         """ returns information """
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """ returns size """
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """ sets size of square """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 1:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
