3641b58aecf5b7fd500b41ec88ca7ec6672fcf34
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-27 08:05:12 -0700

    .

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -45,6 +45,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns instance with attr set"""
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
